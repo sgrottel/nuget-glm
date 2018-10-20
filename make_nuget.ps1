a269d5813247b6837ec33bb1fb990c6172760404
SGrottel <webmaster@sgrottel.de>
2018-10-20 13:34:11 +0200

    Adds explanation the last number of the package version number. Adds release notes extracted from official readme.md

--- make_nuget.ps1
+++ make_nuget.ps1
@@ -10,11 +10,20 @@ else { throw "Failed to parse minor version number from header." }
 if ($header -match '(?m)^#define\s+GLM_VERSION_PATCH\s+(\d+)\s*$') { $verPatch = $Matches[1] }
 else { throw "Failed to parse patch version number from header." }
 if ($header -match '(?m)^#define\s+GLM_VERSION_REVISION\s+(\d+)\s*$') { $verRevision = $Matches[1] }
-else { throw "Failed to parse patch version number from header." }
+else { throw "Failed to parse revision version number from header." }
 
-$verBuild = $verRevision * 100 +$verBuild
+$verBuild = ([int]$verRevision) * 100 + $verBuild
 $version = "$verMajor.$verMinor.$verPatch.$verBuild"
 
 $nuspec = [System.IO.Path]::Combine($PSScriptRoot, ".\glm.nuspec")
 
-nuget pack $nuspec -properties version=$version
+$releaseNotes=""
+$readmefile = [System.IO.Path]::Combine($PSScriptRoot, ".\doc\readme.md")
+if (Test-Path $readmefile)
+{
+	$seekVer = [System.Version]::Parse("$verMajor.$verMinor.$verPatch.$verRevision")
+	$readme = gc $readmefile | out-string
+	($readme -split "[^#]##[^#]") | Select-String -Pattern "^Release Notes" | foreach { $_ -split "[^#]###[^#]" } | Select-String -Pattern "^\[?GLM\s+(\d[.\d]*\d)[^.\d]" | foreach { if ([System.Version]::Parse($_.Matches.Groups[1].Value) -eq $seekVer) { $releaseNotes = $_ } }
+}
+
+nuget pack $nuspec -properties "version=$version;releaseNotes=$releaseNotes"
